e93e968d30795185679538ee54bc971cc4fd5946
Matt Haines <matthaines@gmail.com>
2018-02-28 15:16:59 -0700

    added codepipeline changes to lambda

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -8,13 +8,26 @@ def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-east-1:514088844947:deployPortfolioTopic')
     
+    # default value for location dictionary
+    location = {
+        "bucketName": "portfoliobuild.barefootcloud.com",
+        "objectKey": "PortfolioBuild.zip"
+    }
     try:
+        # get the codepipeline job object and then we can find the build bucket that
+        # code pipeline is using.
+        job = event.get("CodePipeline.job")
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"] == "MyAppBuild":
+                    location = artifact["location"]["s3Location"]
+                    
+        print "Building portfolio from" + str(location)
         s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
         portfolio_bucket = s3.Bucket('barefootmateo-portfolio.barefootcloud.com')
-        build_bucket = s3.Bucket('portfoliobuild.barefootcloud.com')
+        build_bucket = s3.Bucket(location["bucketName"])
         portfolio_zip = StringIO.StringIO()
-        
-        build_bucket.download_fileobj('PortfolioBuild.zip', portfolio_zip)
+        build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
     
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
@@ -25,6 +38,12 @@ def lambda_handler(event, context):
     
         print 'Job done'
         topic.publish(Subject="Portfolio Deployed", Message="Portfolio deployed successfully.")
+        
+        # Log the job ID in codepipeline if we're using it
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
+            
     except:
         topic.publish(Subject="Portfolio Deploy Failed", Message="Failed to deploy portfolio")
         raise
