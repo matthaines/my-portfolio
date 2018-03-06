6cec526eb80102b1e255b2b2d483d915d5d149fb
Matt Haines <matthaines@gmail.com>
2018-03-06 15:52:32 -0700

    aargh

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import 'react-dom'
 
-console.log("loaded react-dom");
+console.log("loaded react-dom ... testing ES6 ...");
+const arr = [1, 2, 3];
+const iAmJavascriptES6 = () => console.log(...arr);
+window.iAmJavascriptES6 = iAmJavascriptES6;
