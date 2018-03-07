49dd89e72dc048d21c2bed8107c2f5b0aaf8bbfc
Matt Haines <matthaines@gmail.com>
2018-03-06 17:34:33 -0700

    factored work bubbles out of index.html and created react component to render them

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
-import 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work'
 
-console.log("loaded react-dom ... testing ES6 ...");
-const arr = [1, 2, 3];
-const iAmJavascriptES6 = () => console.log(...arr);
-window.iAmJavascriptES6 = iAmJavascriptES6;
+console.log("React Online ...");
+
+const myWork = [
+  {
+    'title': "Code Example",
+    'image': {
+      'desc': "example screenshot of a project involving code",
+      "src": "images/example1.png",
+      "comment": ""
+    }
+  },
+  {
+    'title': "Cat Example",
+    'image': {
+      'desc': "example screenshot of a project involving cats",
+      "src": "images/example3.png",
+      "comment": `“Bengal cat” by roberto shabs is licensed under CC BY 2.0
+                  https://www.flickr.com/photos/37287295@N00/2540855181`
+    }
+  },
+  {
+    'title': "Serverless Example",
+    'image': {
+      'desc': "A serverless portfolio",
+      "src": "images/example2.png",
+      "comment": ""
+    }
+  }
+]
+
+ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
